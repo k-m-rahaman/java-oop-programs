5f01d8510e593a838d85f375e9eca68fb9656122
k-m-rahaman <kmrofficial786@gmail.com>
2026-06-03 05:05:24 +0530

    Added inheritance example

---
+++ InheritanceExample.java
@@ -0,0 +1,14 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {}
+
+public class InheritanceExample {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
